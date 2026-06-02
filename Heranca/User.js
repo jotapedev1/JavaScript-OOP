682ffd13c6678910facc45a24fd2120c0a0609e4
jotapedev1 <jotape5195@gmail.com>
2026-06-02 16:25:59 -0300

    Getters and Setters

--- Heranca/User.js
+++ Heranca/User.js
@@ -14,10 +14,40 @@ export default class User{
     getUserInfo() {
         console.log(`Name: ${this.#name}, Email: ${this.#email}, Birthday: ${this.#birthday}, Role: ${this.#role}, Active: ${this.#active}`);
     }
-    
+    get name() {
+        return this.#name;
+    }
+    set name(newName) {
+        this.#name = newName;
+    }   
+    get email() {
+        return this.#email;
+    }
+    set email(newEmail) {
+        this.#email = newEmail;
+    }
+    get birthday() {
+        return this.#birthday;
+    }
+    set birthday(newBirthday) {
+        this.#birthday = newBirthday;
+    }
+    get role() {
+        return this.#role;
+    }
+    set role(newRole) {
+        this.#role = newRole;
+    }
+    get active() {
+        return this.#active;
+    }
+    set active(newActive) {
+        this.#active = newActive;
+    }
+
 }
 
-const user1 = new User("João", "j@j", "07/12/2006");
+//const user1 = new User("João", "j@j", "07/12/2006");
 //user1.getUserInfo();
 
 //console.log(user1.isPrototypeOf(User)); // false
